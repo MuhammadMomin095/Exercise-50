9266da67dfedc17c11458c89a4a4fae88ab9f49c
Muhammad Momin <160387789+MuhammadMomin095@users.noreply.github.com>
2024-03-22 00:11:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+var myIdeolDay = "My ideol day would involve:\n1. When I meet Friends\n2. Spending a few hours playing game\n3. When I watch movies in night with friends.";
+console.log(myIdeolDay);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let myIdeolDay = `My ideol day would involve:
+1. When I meet Friends
+2. Spending a few hours playing game
+3. When I watch movies in night with friends.`;
+
+console.log(myIdeolDay);
